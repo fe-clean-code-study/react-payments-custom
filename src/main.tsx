f53e7e746d8776d7a28ed545095d0a32cc496e1f
Suyeon <msjgr2801@gmail.com>
2024-07-19 17:45:29 +0900

    style: 기본 여백 스타일 없애기

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './App.tsx';
+import './styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
