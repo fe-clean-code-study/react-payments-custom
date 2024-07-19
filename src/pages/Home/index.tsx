d216ca7cbb68e301113bbf6679a934704ac8ca5a
Suyeon <msjgr2801@gmail.com>
2024-07-19 17:46:19 +0900

    feat: main 페이지에서 Modal 관리하기

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,25 @@
-import { Button } from '../../components';
-import { useNavigate } from 'react-router-dom';
+import { Button, Modal } from '../../components';
+import { Outlet, useNavigate } from 'react-router-dom';
 import * as S from './index.style';
+import { useModal } from '../../hooks';
 
 const Home = () => {
   const navigate = useNavigate();
+  const { isOpen, open } = useModal();
 
   return (
     <S.Container>
       <Button
         onClick={() => {
+          open();
           navigate('/payments');
         }}
       >
         <S.ButtonLabel>열기</S.ButtonLabel>
       </Button>
+      <Modal isOpen={isOpen}>
+        <Outlet />
+      </Modal>
     </S.Container>
   );
 };
