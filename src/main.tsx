3f1b2d82f5fbdca3911c52ff591e6ca946456c80
Suyeon <msjgr2801@gmail.com>
2024-07-19 15:44:15 +0900

    chore: 사용하지 않는 경로 삭제하기

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './App.tsx';
-import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
