b676874b9d15dc36d81314d63f422d105affb16f
Suyeon <msjgr2801@gmail.com>
2024-07-19 15:42:38 +0900

    feat: ThemeProvider 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from '@emotion/react';
 import router from './router/router';
 import { RouterProvider } from 'react-router-dom';
+import theme from './styles/theme';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+import { Theme } from '@emotion/react';
+import color from './color';
+
+const theme: Theme = {
+  color,
+};
+
+export default theme;
