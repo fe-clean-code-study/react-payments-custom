b7a4527002dba7e3919522825b75a576dbcd59f8
Suyeon <msjgr2801@gmail.com>
2024-07-19 16:20:22 +0900

    feat: Theme 에 컬러 추가하기

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,3 +1,7 @@
-const color = {};
+const color = {
+  white: '#ffffff',
+  grayBackground: '#e5e5e5',
+  mainGreen: '#04C09E',
+};
 
 export default color;
--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,6 +1,12 @@
 import '@emotion/react';
-import { Theme as CustomTheme } from './theme';
+import color from './color';
+
+export type Color = typeof color;
 
 declare module '@emotion/react' {
-  export interface Theme extends CustomTheme {}
+  export interface Theme extends color {
+    color: {
+      [key in keyof Color]: string;
+    };
+  }
 }
