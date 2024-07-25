42e8604f979205c8aacbbf25bd02fc75fe5a2784
Suyeon <msjgr2801@gmail.com>
2024-07-25 15:48:13 +0900

    refactoring: Input컴포넌트 리팩토링하기
    - 불필요한 props를 기본 InputElement타입으로 받기

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,59 +1,37 @@
-import {
-  ChangeEvent,
-  HTMLInputTypeAttribute,
-  forwardRef,
-  useEffect,
-  useState,
-} from 'react';
+import { ChangeEvent, ComponentProps, forwardRef, useState } from 'react';
+
 import * as S from './index.style';
 
-interface InputProps {
-  onChange?: (value: string) => void;
-  onComplete?: () => void;
-  onBlur?: () => void;
-  defaultValue?: string;
-  maxLength?: number;
-  placeholder?: string;
-  type?: HTMLInputTypeAttribute;
+export interface InputProps extends ComponentProps<'input'> {
+  onComplete?: (target?: HTMLInputElement) => void;
   textAlign?: 'center' | 'left';
   onlyNumber?: boolean;
-  readonly?: boolean;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
   (
-    {
-      onChange,
-      onComplete,
-      onBlur,
-      defaultValue = '',
-      maxLength,
-      onlyNumber = false,
-      textAlign = 'left',
-      placeholder = '',
-      type = 'text',
-      readonly = false,
-    },
+    { onComplete, onChange, onlyNumber = false, textAlign = 'left', ...props },
     ref,
   ) => {
-    const [value, setValue] = useState<string>(defaultValue);
+    const [value, setState] = useState<string>('');
 
-    useEffect(() => {
-      setValue(defaultValue);
-      onChange && onChange(defaultValue);
-    }, [onChange, defaultValue]);
+    const handleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+      const { target } = event;
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-      const newValue = event.target.value;
-      if (onlyNumber && /^\d*$/.test(newValue)) {
-        setValue(newValue);
-        onChange && onChange(newValue);
-      } else if (!onlyNumber) {
-        setValue(newValue);
-        onChange && onChange(newValue);
-      }
-      if (maxLength && maxLength === newValue.length) {
-        onComplete && onComplete();
+      if (target instanceof HTMLInputElement) {
+        const { maxLength } = target;
+        const newValue = event.target.value;
+
+        if (onlyNumber && isNaN(Number(newValue))) {
+          return;
+        }
+        if (!onlyNumber || (onlyNumber && !isNaN(Number(newValue)))) {
+          onChange && onChange(event);
+          setState(newValue);
+        }
+        if (maxLength === newValue.length) {
+          onComplete && onComplete(event.target);
+        }
       }
     };
 
@@ -61,13 +39,11 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       <S.Input
         ref={ref}
         textAlign={textAlign}
-        maxLength={maxLength}
-        type={type}
         value={value}
-        onBlur={onBlur}
-        onChange={handleChange}
-        placeholder={placeholder}
-        readOnly={readonly}
+        onChange={(event) => {
+          handleChangeInput(event);
+        }}
+        {...props}
         autoComplete='new-password'
       />
     );
