a2538ce9c6fc4dc36f1aaf8e03a8a028a4290b36
seeyoujeong <chan3358@naver.com>
2024-07-26 14:08:47 +0900

    design: 카드 삭제 버튼 위치 수정

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -13,9 +13,24 @@ function CardListPage() {
   return (
     <div className="app">
       <h2 className="page-title mb-10">보유 카드</h2>
-      <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "15px",
+          paddingBottom: "50px",
+        }}
+      >
         {cardInfoList.map((cardInfo) => (
-          <div key={cardInfo.id} style={{ position: "relative" }}>
+          <div
+            key={cardInfo.id}
+            style={{
+              position: "relative",
+              width: "208px",
+            }}
+          >
             <Card
               type="filled"
               size="small"
@@ -24,7 +39,16 @@ function CardListPage() {
             />
             <button
               className="pointer"
-              style={{ position: "absolute", top: "-10px", right: "50px" }}
+              style={{
+                position: "absolute",
+                top: "-10px",
+                right: "-10px",
+                width: "25px",
+                height: "25px",
+                backgroundColor: "#e5e5e5",
+                border: "none",
+                borderRadius: "50%",
+              }}
               onClick={() => handleDeleteCard(cardInfo.id)}
             >
               X
