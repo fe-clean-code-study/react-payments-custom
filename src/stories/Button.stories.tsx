3445101aee643d6cae844ddfb501a63f6ba6a301
Suyeon <msjgr2801@gmail.com>
2024-07-26 16:32:54 +0900

    feat: Button 컴포넌트 스토리북 등록하기

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from '@emotion/react';
+import { Button } from '~/components';
+import { ButtonProps } from '~/components/Button';
+import theme from '~/styles/theme';
+
+export default {
+  title: 'components/Button',
+  component: Button,
+};
+
+export const Default = (args: ButtonProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button {...args}>test</Button>
+    </ThemeProvider>
+  );
+};
