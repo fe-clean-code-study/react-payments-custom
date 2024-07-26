fd778ec46724e5a506d73d937025beb6b8a6321e
seeyoujeong <chan3358@naver.com>
2024-07-26 15:19:05 +0900

    feat: Button 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+interface ButtonProps {
+  type?: "submit" | "reset" | "button";
+  disabled?: boolean;
+  onClick: () => void;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+function Button({
+  type = "button",
+  disabled = false,
+  children,
+  onClick,
+  className,
+  style,
+}: React.PropsWithChildren<ButtonProps>) {
+  return (
+    <button
+      className={className}
+      style={{
+        border: "none",
+        margin: 0,
+        padding: 0,
+        backgroundColor: "transparent",
+        cursor: "pointer",
+        fontSize: "1rem",
+        ...style,
+      }}
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Modal } from "./Modal";
 export { default as Icon } from "./Icon";
 export { default as CardInfoEdit } from "./CardInfoEdit";
 export { default as CardNicknameEdit } from "./CardNicknameEdit";
+export { default as Button } from "./Button";
