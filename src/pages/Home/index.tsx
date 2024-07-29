0dc3a849e9cd74be3c544ab62e560f5dc7e88618
Suyeon <msjgr2801@gmail.com>
2024-07-29 16:33:50 +0900

    fix: HomePage 에러 수정
    - 분리한 Deembackground 와 Card 컴포넌트 코드를 반영하지 않아서 생긴 문제 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,12 @@ import { Outlet, useNavigate } from 'react-router-dom';
 
 import * as S from './index.style';
 
-import { Button, Modal } from '~/components';
+import { Button, Card, DeemBackground } from '~/components';
 import { useModal } from '~/hooks';
 
 const Home = () => {
   const navigate = useNavigate();
-  const { isOpen, open } = useModal();
+  const { isOpen, open } = useModal(false);
 
   const handleClickOpenButton = () => {
     open();
@@ -19,11 +19,15 @@ const Home = () => {
       <Button onClick={handleClickOpenButton}>
         <S.ButtonLabel>열기</S.ButtonLabel>
       </Button>
-      <Modal isOpen={isOpen}>
-        <S.ContentContainer>
-          <Outlet />
-        </S.ContentContainer>
-      </Modal>
+      {isOpen && (
+        <DeemBackground>
+          <Card>
+            <S.ContentContainer>
+              <Outlet />
+            </S.ContentContainer>
+          </Card>
+        </DeemBackground>
+      )}
     </S.Container>
   );
 };
