66c5f2ee55e502ecd2afbe4d87b81888607f0e36
Suyeon <msjgr2801@gmail.com>
2024-07-29 16:37:49 +0900

    feat: AddCreditCard 컴포넌트 추가하기

---
+++ src/components/AddCreditCard/index.tsx
@@ -0,0 +1,24 @@
+import { PropsWithChildren } from 'react';
+
+import * as S from './index.style';
+import GoogleIcon from '../GoogleIcon';
+
+interface AddCreditCardProps extends PropsWithChildren {
+  onClick?: () => void;
+}
+
+const AddCreditCard = ({ onClick }: AddCreditCardProps) => {
+  const handleClickCard = () => {
+    onClick && onClick();
+  };
+
+  return (
+    <S.Container>
+      <S.CreditCard onClick={handleClickCard}>
+        <GoogleIcon name='add' />
+      </S.CreditCard>
+    </S.Container>
+  );
+};
+
+export default AddCreditCard;
