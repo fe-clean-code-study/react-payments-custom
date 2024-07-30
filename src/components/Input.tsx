d036f784a6d3b3aa8d1f5e39d6124bcaf8048fdf
seeyoujeong <chan3358@naver.com>
2024-07-30 17:12:04 +0900

    refactor: Input 컴포넌트 props 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,36 +3,14 @@ import { forwardRef } from "react";
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  (
-    {
-      type = "text",
-      value,
-      placeholder,
-      className,
-      disabled = false,
-      required = false,
-      min,
-      max,
-      minLength,
-      maxLength,
-      onChange,
-    },
-    ref
-  ) => {
+  ({ type = "text", disabled = false, required = false, ...props }, ref) => {
     return (
       <input
         ref={ref}
         type={type}
-        value={value}
-        placeholder={placeholder}
-        className={className}
         disabled={disabled}
         required={required}
-        min={min}
-        max={max}
-        minLength={minLength}
-        maxLength={maxLength}
-        onChange={onChange}
+        {...props}
       />
     );
   }
