544d2da2be145c6e581939e88b2a004f043f2a07
seeyoujeong <chan3358@naver.com>
2024-07-31 22:01:11 +0900

    refactor: element가 유효한지 검사 추가

--- src/hooks/useFocusNext.ts
+++ src/hooks/useFocusNext.ts
@@ -12,12 +12,14 @@ const useFocusNext = (order: string[]) => {
 
   const focusNext = (
     element: HTMLInputElement | null,
-    shouldMoveFocus: (value: string | undefined) => boolean
+    shouldMoveFocus: (value: string) => boolean
   ) => {
-    const currentIndex = order.findIndex((name) => element?.name === name);
+    if (!element) return;
+
+    const currentIndex = order.findIndex((name) => element.name === name);
     const nextIndex = currentIndex + 1;
 
-    if (shouldMoveFocus(element?.value) && nextIndex < order.length) {
+    if (shouldMoveFocus(element.value) && nextIndex < order.length) {
       const nextName = order[nextIndex];
       inputRefs.current[nextName]?.focus();
     }
