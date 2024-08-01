d6583e7af49e148cbc87a7c0af4adf650f2546d0
seeyoujeong <chan3358@naver.com>
2024-08-01 10:21:40 +0900

    refactor: 상수 폴더 생성하고 분리

--- src/components/CardInfoEdit.tsx
+++ src/components/CardInfoEdit.tsx
@@ -1,3 +1,4 @@
+import { CARD_NAME_LIST } from "../constants";
 import { useCardForm, useModal } from "../hooks";
 import {
   Card,
@@ -8,17 +9,6 @@ import {
   CardUsernameInput,
 } from "./card";
 import { Button, Icon, Modal, Title } from "./common";
-
-const CARD_NAME_LIST = [
-  { name: "찬욱 카드", color: "#E24141" },
-  { name: "효리 카드", color: "#547CE4" },
-  { name: "수연 카드", color: "#73BC6D" },
-  { name: "세진 카드", color: "#DE59B9" },
-  { name: "진경 카드", color: "#94DACD" },
-  { name: "종길 카드", color: "#E76E9A" },
-  { name: "건우 카드", color: "#F37D3B" },
-  { name: "혜성 카드", color: "#FBCD58" },
-];
 
 interface CardInfoEditProps {
   cardForm: ReturnType<typeof useCardForm>;
