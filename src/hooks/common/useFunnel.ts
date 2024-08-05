032ec5880b4e99bc81f4f1c1407307aadcdabab6
hyesung99 <4538asd@naver.com>
2024-08-05 23:29:09 +0900

    refactor : useFunnel 제네릭 수정

--- src/hooks/common/useFunnel.ts
+++ src/hooks/common/useFunnel.ts
@@ -1,15 +1,52 @@
-import { PropsWithChildren, useCallback, useMemo, useState } from "react";
+import {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 
 interface StepProps {
   name: string;
 }
 
-export const useFunnel = () => {
-  const [currentStep, setCurrentStep] = useState<string>();
+interface UseFunnelOptions<T extends string> {
+  stepQueryKey?: string;
+  initialStep?: T;
+  onStepChange?: (step: T) => void;
+}
 
-  const setStep = useCallback((step: string) => {
-    setCurrentStep(step);
-  }, []);
+export const useFunnel = <T extends string>(
+  steps: readonly T[],
+  options: UseFunnelOptions<T> = {}
+) => {
+  const {
+    stepQueryKey = "funnel-step",
+    initialStep = steps[0],
+    onStepChange,
+  } = options;
+
+  const [currentStep, setCurrentStep] = useState<T>(initialStep);
+
+  const setStep = useCallback(
+    (step: T) => {
+      setCurrentStep(step);
+      if (onStepChange) {
+        onStepChange(step);
+      }
+    },
+    [onStepChange]
+  );
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const stepFromQuery = params.get(stepQueryKey);
+    if (stepFromQuery && steps.includes(stepFromQuery as T)) {
+      setStep(stepFromQuery as T);
+    } else {
+      setStep(initialStep);
+    }
+  }, [stepQueryKey, initialStep, setStep, steps]);
 
   const Funnel = useMemo(() => {
     const FunnelComponent = ({ children }: PropsWithChildren) => {
