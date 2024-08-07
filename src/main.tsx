783f4e911c1663de895683eee82a402c9dd1e532
Suyeon <msjgr2801@gmail.com>
2024-08-07 15:57:51 +0900

    refactoring: 변경된 App 파일로 폴더 경로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './app/App';
 import './styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
