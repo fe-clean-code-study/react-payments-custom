8dd9740ac6f6d86f55b894c977a506633370d7b1
Suyeon <msjgr2801@gmail.com>
2024-08-13 11:33:54 +0900

    style: 기본 Field 스타일 변경
    - input 패딩 넓히기
    - input 컬러 수정하기
    - Label과 에러 메세지 폰트 크기 감소시키기

--- src/shared/ui/Field/Field.style.ts
+++ src/shared/ui/Field/Field.style.ts
@@ -5,21 +5,26 @@ export const Root = styled.div`
 
   display: flex;
   flex-direction: column;
-  gap: 5px;
+  gap: 7px;
 
-  border: 1px solid ${({ theme }) => theme.color.gray100};
   border-radius: 5px;
 `;
 
 export const Input = styled.input`
-  padding: 10px;
+  padding: 15px 10px;
   outline: none;
   border: 1px solid ${({ theme }) => theme.color.gray100};
   border-radius: 5px;
+  font-size: 16px;
+  color: ${({ theme }) => theme.color.green500};
 `;
 
 export const ErrorText = styled.p`
   color: ${({ theme }) => theme.color.red500};
+  font-size: 12px;
 `;
 
-export const Label = styled.label``;
+export const Label = styled.label`
+  font-size: 12px;
+  color: ${({ theme }) => theme.color.gray500};
+`;
