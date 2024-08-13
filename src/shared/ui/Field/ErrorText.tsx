015d7b998150fc6e5f23b3159150bc01431007e7
Suyeon <msjgr2801@gmail.com>
2024-08-13 12:17:53 +0900

    feat: Field.ErrorText 에 에러 값 설정하기

--- src/shared/ui/Field/ErrorText.tsx
+++ src/shared/ui/Field/ErrorText.tsx
@@ -4,18 +4,26 @@ import * as S from './Field.style';
 
 export interface ErrorTextProps extends ComponentProps<'p'> {
   style?: CSSProperties;
+  isShow?: boolean;
 }
 
-const ErrorText = ({ style, children, ...props }: ErrorTextProps) => {
+const ErrorText = ({
+  style,
+  isShow = false,
+  children,
+  ...props
+}: ErrorTextProps) => {
   return (
-    <S.ErrorText
-      style={{
-        ...style,
-      }}
-      {...props}
-    >
-      {children}
-    </S.ErrorText>
+    isShow && (
+      <S.ErrorText
+        style={{
+          ...style,
+        }}
+        {...props}
+      >
+        {children}
+      </S.ErrorText>
+    )
   );
 };
 
