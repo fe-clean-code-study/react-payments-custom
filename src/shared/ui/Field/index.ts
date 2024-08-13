8a51dc18a72d6de6af7ad6a56d504260bf2d4d5f
Suyeon <msjgr2801@gmail.com>
2024-08-13 11:58:59 +0900

    chore: InputWrapper 내보내기 추가

--- src/shared/ui/Field/index.ts
+++ src/shared/ui/Field/index.ts
@@ -1,5 +1,6 @@
 import ErrorText from './ErrorText';
 import Input from './Input';
+import InputWrapper from './InputWrapper';
 import Label from './Label';
 import Root from './Root';
 
@@ -7,10 +8,11 @@ export type { RootProps } from './Root';
 export type { LabelProps } from './Label';
 export type { InputProps } from './Input';
 export type { ErrorTextProps } from './ErrorText';
-
+export type { InputWrapperProps } from './InputWrapper';
 export const Field = {
   Root,
   Label,
   Input,
   ErrorText,
+  InputWrapper,
 };
