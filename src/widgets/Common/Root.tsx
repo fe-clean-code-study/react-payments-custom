6d40549879a2017bfa7feb88657fb42556644138
Suyeon <msjgr2801@gmail.com>
2024-08-16 18:33:48 +0900

    feat: Layout에 Modal 기능 추가하기

--- src/widgets/Common/Root.tsx
+++ src/widgets/Common/Root.tsx
@@ -2,12 +2,15 @@ import { ComponentProps } from 'react';
 
 import * as S from './Common.style';
 
+import { Modal } from '~/shared/ui';
+
 export interface RootProps extends ComponentProps<'div'> {}
 
 const Root = ({ children }: RootProps) => {
   return (
     <S.Root isOpen={true} onClose={() => {}}>
-      {children}
+      <Modal.Overlay />
+      <Modal.Content>{children}</Modal.Content>
     </S.Root>
   );
 };
