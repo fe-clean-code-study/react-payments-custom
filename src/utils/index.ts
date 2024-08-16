5bf4906b23c2867825f89d00e8af5a5b1f436255
seeyoujeong <chan3358@naver.com>
2024-08-16 15:46:12 +0900

    chore: 불필요한 파일 삭제

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export { default as getRandomId } from "./getRandomId";
-export { default as isPositiveInteger } from "./isPositiveInteger";
 export { default as filterDigits } from "./filterDigits";
