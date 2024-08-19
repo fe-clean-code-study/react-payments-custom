ef454ab20c61825911ec90bc39666259d7decf15
Suyeon <msjgr2801@gmail.com>
2024-08-19 15:13:50 +0900

    fix: Number Field 유효성 검사 안 하는 문제 수정하기

--- src/features/RegistCard/ui/NumbersField/Root.tsx
+++ src/features/RegistCard/ui/NumbersField/Root.tsx
@@ -6,7 +6,7 @@ import { cardRegsitFormValidate, validateHelper } from '~/features/utils';
 import { Field } from '~/shared/ui';
 
 const Root = () => {
-  const { register, getFieldState, setFocus } = useFormContext();
+  const { register, getFieldState, formState, setFocus } = useFormContext();
   const names = ['numbers.0', 'numbers.1', 'numbers.2', 'numbers.3'];
 
   return (
@@ -18,18 +18,20 @@ const Root = () => {
             key={index}
             maxLength={4}
             onComplete={() => {
-              console.log(names[index + 1]);
               index < names.length - 1 && setFocus(names[index + 1]);
             }}
             {...register(name, {
               maxLength: 4,
+              required: true,
               validate: (value) =>
                 validateHelper(cardRegsitFormValidate['numbers'], value),
             })}
           />
         ))}
       </S.InputWrapper>
-      <Field.ErrorText isShow={getFieldState('numbers').error !== undefined}>
+      <Field.ErrorText
+        isShow={getFieldState('numbers', formState).error !== undefined}
+      >
         카드 번호를 입력해주세요.
       </Field.ErrorText>
     </Field.Root>
