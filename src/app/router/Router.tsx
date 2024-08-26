814b9c7f3746ab32dd957fb8835da8b0373b3704
Suyeon <msjgr2801@gmail.com>
2024-08-26 21:36:04 +0900

    feat: Router 경로 수정
    - manage-card로 경로 변경
    - CardAlias 페이지 경로 추가

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 
-// import CardAlias from '~/pages/CardAlias';
+import { CardAlias } from '~/pages/CardAlias';
 import { CardRegist } from '~/pages/CardRegist';
 import { ManageCard } from '~/pages/ManageCard';
 
 const Router = createBrowserRouter([
   { element: <Navigate to='/card-list' />, path: '/' },
-  { element: <ManageCard />, path: '/card-list' },
+  { element: <ManageCard />, path: '/manage-card' },
   { element: <CardRegist />, path: '/card-regist' },
-  // { element: <CardAlias />, path: '/card-alias/:id' },
+  { element: <CardAlias />, path: '/card-alias/:id' },
 ]);
 
 export default Router;
